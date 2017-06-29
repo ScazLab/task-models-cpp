1c03bb92a9a54b52de50a241834dc57e9bb60554
Sarim Abbas <sarim.stays@gmail.com>
2017-06-29 14:16:59 -0400

    fixes for utils, added test

--- lib/task-models/include/utils.h
+++ lib/task-models/include/utils.h
@@ -3,8 +3,9 @@
 
 /**
  * sums a matrix and checks if approximately equal to 1.0
- * @param  array an eigen matrix
- * @param  name  name of the array
- * @return       true/false if success/failure
+ * @param  array   an eigen matrix
+ * @param  name    name of array
+ * @param  epsilon optional interval for check
+ * @return         true/false if success/failure
  */
-bool assertNormal(Eigen::MatrixXd array, std::string name="array");
+bool isNormal(Eigen::MatrixXd array, double epsilon=1e-10, std::string name="array");
--- lib/task-models/src/utils.cpp
+++ lib/task-models/src/utils.cpp
@@ -1,21 +1,19 @@
 #include <utils.h>
-#include <typeinfo>
 
 using namespace std;
 using Eigen::MatrixXd;
 
-bool assertNormal(MatrixXd array, string name)
+bool isNormal(MatrixXd array, double epsilon, string name)
 {
 	// e.g:
-	// [1.0, 0] => [1.0]
-	// [0.5, 0.3 => [1.0]
+	// [1.0, 0] => 1.0
+	// [0.5, 0.3 => 1.0
 	//  0.1, 0.1]
-	MatrixXd sumMatrix(1,1);
-	sumMatrix(0,0) = array.sum();
+	double sum = array.sum();
 
 	// e.g:
-	// [1.0] == [1.]
-	if(not sumMatrix.isApprox(MatrixXd::Constant(1,1,1.)))
+	// 1.0 == 1.
+	if(sum > 1.0 + epsilon || sum < 1.0 - epsilon)
 	{
 		cout << "[Error] Probabilities in " << name << " should sum to 1." << endl;
 		return false;
